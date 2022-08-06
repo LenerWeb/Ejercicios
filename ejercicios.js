564b2a961da0cd8404da8f618bf0f30b9e6a4c5c
lenerweb <lener20@hotmail.com>
2022-08-06 00:11:53 -0500

    ingresar valores en promt

--- ejercicios.js
+++ ejercicios.js
@@ -1,83 +1,130 @@
-//2 argumentos
-/* let num1 = prompt("SUMAR - Ingrese Primer Numero");
-let num2 = prompt("SUMAR - Ingrese Segundo Numero")
 
-let sumar = +num1 + +num2;
+function CallMenu(){
+    let nro_ejercicio = parseInt(
+        prompt("Ingrese el numero de ejercicio que quiera ejecuttar: \r\n 1. Suma. \r\n 2. Promedio de Exmenes \r\n 3. Calcular el área del rectangulo \r\n 4. Calcular el área del triangulo \r\n 5. Calcular el área del circulo \r\n 6. Calcular salario semanal \r\n 7. Convertir pulgadas \r\n 8. Cambiar a dolares")
+    );
+    if (isNaN(nro_ejercicio)){
+        alert("Porfavor ingresa valores numericos");
 
-document.write("el resultado final es: " ,sumar) */
-
-//notas de 4 examenes
-/* let a = prompt("Ingresar nota1")
-let b = prompt("Ingresar nota2")
-let c = prompt("Ingresar nota3")
-let d = prompt("Ingresar nota4")
-
-function sumarNotas(a, b, c, d) {
-    return (+a + +b + +c + +d)/sumarNotas.length
-}
-let promedio = sumarNotas (a, b, c, d)
-
-console.log("Promedio final", promedio) */
-
-
-//calcular area de un rectangulo
-
-/* let base = prompt ("Ingrese Base del rectangulo")
-let altura = prompt ("Ingrese Altura del rectangulo")
-
-let area = function (base, altura) {
-    return (parseInt(base)*parseInt(altura))
-
-}
-console.log("Area del rectangulo; ", area(base, altura)) */
-
-//calcular area de un triangulo
-
-/* let base = prompt ("Ingrese Base del triangulo")
-let altura = prompt ("Ingrese Altura del triangulo")
-
-let area = function (base, altura) {
-    return (parseInt(base)*parseInt(altura))/2
-
-}
-console.log("Area del triangulo: ", area(base, altura)) */
-
-//calcular area de circunferencia
-
-/* let radio = prompt("Ingrese radio del circulo")
-const pi = 3.1416
-
-let areaCirculo = pi*radio*radio
-
-console.log(`Area de una circunferencia con radio ${radio} es: `, areaCirculo) */
-
-//sueldo semanal de un trabajador
-/* let horasTrabajo = prompt("Ingrese horas de trabajo toral en la semana")
-let salarioHora = prompt("Ingrese salario del trabajador por hora")
-
-let salario = horasTrabajo*salarioHora
-
-console.log(`Salario semanal es ${salario} soles`) */
-
-//modista de prendas
-/* const pulg = 0.0254
-let longitudMetros = prompt("Ingrese la meida en metros")
-
-function convertir() {
-    return longitudMetros/pulg
+    }else {
+            MenuEjercicios(nro_ejercicio)
+        }
 }
 
-let pedido = convertir
-console.log(pedido()) */
+function MenuEjercicios(nro_ejercicio) {
+    switch (nro_ejercicio) {
+        case 1:
+            let valor1 = parseFloat(prompt("Ingrese el valor 1 a sumar:"));
+            let valor2 = parseFloat(prompt("Ingrese el valor 2 a sumar:"));
+            alert(ej1_sumaValores(valor1,valor2));
+        break;
 
-//cambio soles a dolares
+        case 2:
+            let ex1 = parseFloat(prompt("Ingrese la primera nota:"));
+            let ex2 = parseFloat(prompt("Ingrese la segunda nota:"));
+            let ex3 = parseFloat(prompt("Ingrese la tercera nota:"));
+            let ex4 = parseFloat(prompt("Ingrese la cuarta nota:"));
+            alert(ej2_calcularPromedio(ex1,ex2,ex3,ex4));
+        break;
 
-/* let costoDolar = 3.78
-let cantidadSoles = prompt("Ingrese la cantidad en soles")
+        case 3:
+            let base_rec = parseFloat(prompt("Ingrese base de rectangulo:"));
+            let altura_rec = parseFloat(prompt("Ingrese altura de rectangulo:"));
+            alert(ej3_calcularAreaRectangulo(base_rec,altura_rec));
+        break;
 
-function cambio() {
-    return cantidadSoles/costoDolar
+        case 4:
+            let base_tri = parseFloat(prompt("Ingrese base de triangulo:"));
+            let altura_tri = parseFloat(prompt("Ingrese altura de triangulo:"));
+            alert(ej4_calcularAreaTriangulo(base_tri,altura_tri));
+        break;
+
+        case 5:
+            const pi_cir = 3.1416;
+            let radio_cir = parseFloat(prompt("Ingrese radio del circulo:"));
+            alert(ej5_calcularAreaCirculo(pi_cir,radio_cir));
+        break;
+
+        case 6:
+            let hr_sem = parseFloat(prompt("Ingrese total horas trabajadas en la semana:"));
+            let salario_hr = parseFloat(prompt("Ingrese el salario por hora:"));
+            alert(ej6_calcularSalarioSemanal(hr_sem,salario_hr));
+        break;
+
+        case 7:
+            const val_pulg = 0.0254;
+            let cant_mts = parseFloat(prompt("Ingrese cantidad de metros a convertir:"));
+            alert(ej7_calcularCantidadMetros(val_pulg,cant_mts));
+        break;
+
+        case 8:
+            const cot_dolar = 3.78;
+            let cant_soles = parseFloat(prompt("Ingrese cantidad de soles para cambiar:"));
+            alert(ej8_calcularCambioDolares(cot_dolar,cant_soles));
+        break;
+    }
 }
 
-let divisas = cambio
-console.log("Su cambio de soles a dolares es:" ,divisas()) */
+function ej1_sumaValores(valor1,valor2){
+    if (isNaN(valor1) || isNaN(valor2)){
+        return "Porfavor ingrese valores";
+    }else{
+        return `La suma de los valores es: ${valor1+valor2}`;
+    }
+}
+
+function ej2_calcularPromedio(ex1,ex2,ex3,ex4){
+    if (isNaN(ex1) || isNaN(ex2) ||isNaN(ex3)||isNaN(ex4)){
+        return "Porfavor ingrese valores";
+    }else{
+        return `El promedio de notas es: ${(ex1 + ex2 + ex3 + ex4)/4}`;
+    }
+}
+
+function ej3_calcularAreaRectangulo(base_rec,altura_rec){
+    if (isNaN(base_rec) || isNaN(altura_rec)){
+        return "Porfavor ingrese valores";
+    }else{
+        return "El area del rectangulo es: " +base_rec*+altura_rec;
+    }
+}
+
+function ej4_calcularAreaTriangulo(base_tri,altura_tri){
+    if (isNaN(base_tri) || isNaN(altura_tri)){
+        return "Porfavor ingrese valores";
+    }else{
+        return "El area del triangulo es: " +base_tri*+altura_tri/2;
+    }
+}
+
+function ej5_calcularAreaCirculo(pi_cir,radio_cir){
+    if (isNaN(pi_cir)|| isNaN(radio_cir)){
+        return "Porfavor ingrese valores";
+    }else{
+        return "El area del circulo es: " +radio_cir*+radio_cir*pi_cir;
+    }
+}
+
+function ej6_calcularSalarioSemanal(hr_sem,salario_hr){
+    if (isNaN(hr_sem) || isNaN(salario_hr)){
+        return "Porfavor ingrese valores";
+    }else{
+        return "El salario semanal es: " +hr_sem*+salario_hr;
+    }
+}
+
+function ej7_calcularCantidadMetros(val_pulg,cant_mts){
+    if (isNaN(val_pulg)|| isNaN(cant_mts)){
+        return "Porfavor ingrese valores";
+    }else{
+        return "El cambio de metros a pulgadas es: " +cant_mts/+val_pulg;
+    }
+}
+
+function ej8_calcularCambioDolares(cot_dolar,cant_soles){
+    if (isNaN(cot_dolar)|| isNaN(cant_soles)){
+        return "Porfavor ingrese valores";
+    }else{
+        return "Su cambio de soles a dolares es: " +cant_soles/+cot_dolar;
+    }
+}
